e9522c2311e04edf4044ea23d30589ce4f169bb5
Dimas <dimas11@rocketmail.com>
2013-11-24 07:57:40 +0700

    File tubes 1 yang akan diedit

---
+++ pages/registerkartu.php
@@ -0,0 +1,20 @@
+<script src='scripts/registerkartu.js'></script>
+<div class='formcontainer'>
+	<h2>Pendaftaran Kartu Kredit</h2>
+	<br />
+	<form id='formregisterkartu' method='post'>
+		<span class='formlabel'>No Kartu</span><input type='text' name='nokartu' value=' '><br />
+		<br />
+		
+		<span class='formlabel'>Nama pada Kartu</span><input type='password' name='namapadakartu'><br />
+		<br />
+		
+		<span class='formlabel'>Tanggal Kadaluarasa</span><input type='password' name='tanggalkadaluarsa'><br />
+		<br />
+		
+		<span class='formlabel'>Nama lengkap</span><input type='text' name='name'><br />
+		
+		<br />
+		<input type='submit' name='submit' value='Daftarkan'>
+	</form>
+</div>
---
+++ scripts/registerkartu.js
@@ -0,0 +1,12 @@
+var windowonload = window.onload;
+window.addEventListener('load', function() {
+	var nokartu = document.getElementsByName('nokartu')[0];
+	var namapadakartu = document.getElementsByName('namapadakartu')[0];
+	var tanggalkadaluarsa = document.getElementsByName('tanggalkadaluarsa')
+	var submit = document.getElementsByName('submit')[0];
+	
+	var erroruser5 = document.getElementById('erroruser5');
+	
+	nokartu.focus();
+	
+});
