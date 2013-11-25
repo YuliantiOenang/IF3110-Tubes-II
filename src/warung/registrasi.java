3afbd8a027274cf65fef0988fc6edf73411b71ab
fawwazmuhammad <fawwazmuhammad@gmail.com>
2013-11-25 18:36:28 +0700

    modified

---
+++ src/warung/registrasi.java
@@ -0,0 +1,19 @@
+package warung;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class registrasi extends HttpServlet{
+	
+	//private String message;
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
+		response.setContentType("text/html");
+	 
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + "Test" + "</h1>");
+	}
+	
+}
