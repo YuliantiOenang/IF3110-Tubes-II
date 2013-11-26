853be0599d1f97a4adaf7567710a38f3da9e2201
Iskandar Setiadi <freedom_holicx@yahoo.com>
2013-11-26 18:21:24 +0800

    Update cart and several new templates

--- src/com/frexesc/controller/Detail.java
+++ src/com/frexesc/controller/Detail.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.frexesc.model.BarangBean;
 import com.frexesc.model.KategoriBean;
@@ -26,6 +27,7 @@ public class Detail extends HttpServlet {
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public Detail() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -36,52 +38,59 @@ public class Detail extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		DbConnection dbConnection = new DbConnection();
-		Connection connection = dbConnection.mySqlConnection();
+		HttpSession session = request.getSession();
 
-		int id = 0;
+		if (session.getAttribute("username") == null) {
+			response.sendRedirect("../register.jsp");
+		} else {
+			
+			DbConnection dbConnection = new DbConnection();
+			Connection connection = dbConnection.mySqlConnection();
 
-		if (request.getParameter("id") != null) {
-			id = Integer.valueOf(request.getParameter("id"));
-		}
+			int id = 0;
 
-		try {
-			String query = "SELECT * FROM barang JOIN kategori ON barang.id_kategori=kategori.id AND barang.id="
-					+ id; // Select item based on id
-			ResultSet rs = connection.createStatement().executeQuery(query);
-
-			ArrayList<BarangBean> allResults = new ArrayList<BarangBean>();
-
-			while (rs.next()) {
-				BarangBean barang = new BarangBean(Integer.valueOf(rs
-						.getString("id")), Integer.valueOf(rs
-						.getString("id_kategori")),
-						rs.getString("nama_barang"), rs.getString("gambar"),
-						Integer.valueOf(rs.getString("harga_barang")),
-						rs.getString("keterangan"), Integer.valueOf(rs
-								.getString("jumlah_barang")));
-				allResults.add(barang);
+			if (request.getParameter("id") != null) {
+				id = Integer.valueOf(request.getParameter("id"));
 			}
 
-			String query2 = "SELECT * FROM kategori";
-			ResultSet rs2 = connection.createStatement().executeQuery(query2);
+			try {
+				String query = "SELECT * FROM barang JOIN kategori ON barang.id_kategori=kategori.id AND barang.id="
+						+ id; // Select item based on id
+				ResultSet rs = connection.createStatement().executeQuery(query);
 
-			ArrayList<KategoriBean> allResults2 = new ArrayList<KategoriBean>();
+				ArrayList<BarangBean> allResults = new ArrayList<BarangBean>();
 
-			while (rs2.next()) {
-				KategoriBean kategori = new KategoriBean(Integer.valueOf(rs2
-						.getString("id")), rs2.getString("nama"));
-				allResults2.add(kategori);
+				while (rs.next()) {
+					BarangBean barang = new BarangBean(Integer.valueOf(rs
+							.getString("id")), Integer.valueOf(rs
+							.getString("id_kategori")),
+							rs.getString("nama_barang"), rs.getString("gambar"),
+							Integer.valueOf(rs.getString("harga_barang")),
+							rs.getString("keterangan"), Integer.valueOf(rs
+									.getString("jumlah_barang")));
+					allResults.add(barang);
+				}
+
+				String query2 = "SELECT * FROM kategori";
+				ResultSet rs2 = connection.createStatement().executeQuery(query2);
+
+				ArrayList<KategoriBean> allResults2 = new ArrayList<KategoriBean>();
+
+				while (rs2.next()) {
+					KategoriBean kategori = new KategoriBean(Integer.valueOf(rs2
+							.getString("id")), rs2.getString("nama"));
+					allResults2.add(kategori);
+				}
+
+				request.setAttribute("items", allResults);
+				request.setAttribute("categories", allResults2);
+				RequestDispatcher dispatcher = getServletContext()
+						.getRequestDispatcher("/barang/detail.jsp");
+				dispatcher.forward(request, response);
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-			request.setAttribute("items", allResults);
-			request.setAttribute("categories", allResults2);
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/barang/detail.jsp");
-			dispatcher.forward(request, response);
-
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 
