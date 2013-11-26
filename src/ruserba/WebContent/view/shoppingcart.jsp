b6310b7a1991041b3a9bf640411eecbda1f41581
Ridho Akbarisanto <ridho.akbarisanto@yahoo.com>
2013-11-26 21:43:52 +0700

    mau ngepull punya habibie lagi

--- src/ruserba/WebContent/view/shoppingcart.jsp
+++ src/ruserba/WebContent/view/shoppingcart.jsp
@@ -1,12 +1,58 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ page import="javaModel.Barang" %>
+<%@page import="java.util.ArrayList" %>
 
-</body>
-</html>
+		<h1 class='header'>Shopping Cart</h1>
+<div class='orderdata'>
+			<div class="wrapV_m">
+				<div class='orderdetail'><h4>Order details</h4></div>
+			<div id="contentV_m">
+				<div>
+					<div class='row'>
+						<div class='list_head' id='no'><h6>No</h6></div>
+						<div class='list_head' id='item'><h6>Item</h6></div>
+						<div class='list_head' id='price'><h6>Price</h6></div>
+						<div class='list_head' id='qty'><h6>Qty.</h6></div>
+						<div class='list_head' id='subtotal'><h6>Sub Total</h6></div>
+						<div class='list_head' id='remove'><h6>X</h6></div>
+					</div>
+					<% 	
+						ArrayList <String> arrayn = (ArrayList <String>) request.getAttribute("namabeli");
+						ArrayList <String> arrayc = (ArrayList <String>) request.getAttribute("katbeli");
+						ArrayList <String> arrayp = (ArrayList <String>) request.getAttribute("hargabeli");
+						ArrayList <String> arrayq = (ArrayList <String>) request.getAttribute("jumlahbeli");
+						ArrayList <String> arrayst = (ArrayList <String>) request.getAttribute("stbeli");
+						for(int i = 0; i < arrayn.size(); i++) { %>
+							<div class='row'>
+							<div class='list_body' id='no'><p><%= i + 1 %></p></div>
+							<div class='list_body' id='item'><p><b><%= arrayc.get(i) %> :</b><br/> &nbsp &nbsp &nbsp<%= arrayn.get(i) %></p></div>
+							<div class='list_body' id='price'><p>IDR <%= arrayp.get(i) %></p></div>
+							<div class='list_body' id='qty'><input id='quantity_<%= arrayn.get(i) %>' type='number' onkeyup='cekCart(\"<%= arrayn.get(i) %>\", ".$_SESSION[$item].")' value='".$_SESSION[$item]."'></input></div>
+							<input type='hidden' id='id_barang_".$brg->nama."' value='".$brg->nama."'>
+							<div class='list_body' id='subtotal'><p>IDR <%= arrayst.get(i) %></p></div>
+							<div class='list_body' id='remove'><a href='".$this->makeUrl("barang/delete/".$brg->id)."' title='Remove ".$item." from your Shopping Cart'>x</a></div>
+							</div>
+						<% } %>
+						<div class='row'>
+							<div class='list_foot' id='totallabel'><h6>TOTAL</h6></div>
+							<div class='list_foot' id='total'><p>IDR </p></div>
+						</div>
+					<h2>Free delivery cost. :)</h2>
+				</div>
+			</div></div>
+			<div class='formcontainer'>
+				<div class='buyerdetail'><h4>Term and Condition</h4>
+				<ul>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus neque nisl, aliquam ac mi ut, imperdiet consequat odio. Mauris suscipit laoreet dignissim.</li>
+					<li>Urabitur convallis varius lectus, vitae congue mauris adipiscing eu. Vivamus id ultrices mi. Aenean eget erat id massa fringilla gravida.</li>
+					<li>Aenean eu augue aliquet, congue nisl vitae, mattis quam. Quisque eu urna cursus, semper turpis in, ultricies est.</li>
+					<li>Proin ullamcorper vehicula dolor, volutpat euismod leo cursus varius.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus neque nisl, aliquam ac mi ut, imperdiet consequat odio. Mauris suscipit laoreet dignissim.</li>
+					<li>Urabitur convallis varius lectus, vitae congue mauris adipiscing eu. Vivamus id ultrices mi. Aenean eget erat id massa fringilla gravida.</li>
+					
+				</ul>
+				<a href='profile/credit?redirect=shop/payment'><input type="btn"  name="submit" value="Process to payment" class="button"></a>
+				<a href='kategori'><input type="btn" name="submit" value="Add Item" class="button"></a>
+				</div>
+			</div>
+		</div>
+<script src="js/validasiBarang.js"></script>
---
+++ src/ruserba/src/transactionController/Cart.java
@@ -0,0 +1,136 @@
+package transactionController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javaModel.Barang;
+import javaModel.Kategori;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import databaseLib.DatabaseAdapter;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/cart")
+public class Cart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private DatabaseAdapter DBA = new DatabaseAdapter();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Cart() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		
+		boolean isLogin = true;
+//		if (session.getAttribute("isLogin")!=null)
+//			isLogin = (boolean)session.getAttribute("isLogin");
+//		else
+//			isLogin = false;
+		
+		if (isLogin) {
+			int total = 0;
+			int ind = 1;
+			
+			ArrayList <String> arrayn = new ArrayList<String>();
+			ArrayList <String> arrayc = new ArrayList<String>();
+			ArrayList <String> arrayp = new ArrayList<String>();
+			ArrayList <String> arrayq = new ArrayList<String>();
+			ArrayList <String> arrayst = new ArrayList<String>();
+			if (session.getAttribute("dibeli")!=null) {
+				ArrayList <String> array = (ArrayList<String>) session.getAttribute("dibeli");
+				ind = 1;
+				total = 0;
+				Kategori K = new Kategori(DBA);
+				for (String item : array) {
+					if ((session.getAttribute(item)!=null) && ((int)session.getAttribute(item) > 0)){
+						int jml = (int)session.getAttribute(item);
+						String q = "select * from barang where nama = "+item;
+						String k = "";
+						Barang B = new Barang(DBA);
+						for (int i = 0; i < K.id.size(); i++)
+							if (K.id.get(i).equals(B.id.get(0))) {
+								k = K.nama_kategori.get(i);
+								break;
+							}
+						
+						B.executeQuery(q);
+						
+						int harga = 0;
+						for (int j = 0; j < B.harga.get(0).length(); j ++) {
+							harga = harga * 10 + (int) (B.harga.get(0).charAt(j) - '0');
+						}
+						int subtotal = harga * jml;
+						total += subtotal;
+						ind++;
+						arrayn.add(B.nama.get(0));
+						arrayc.add(k);
+						arrayp.add(B.harga.get(0));
+						arrayq.add(item);
+						arrayst.add(subtotal+"");
+					}
+				}
+			} 
+			request.setAttribute("total_shopping",total);
+			request.setAttribute("namabeli",arrayn);
+			request.setAttribute("katbeli",arrayc);
+			request.setAttribute("hargabeli",arrayp);
+			request.setAttribute("jumlahbeli",arrayq);
+			request.setAttribute("stbeli",arrayst);
+			
+			request.setAttribute("includeJspContent", "shoppingcart.jsp");
+			request.getRequestDispatcher("/view/layout.jsp").forward(request, response);	
+		} else {
+			response.sendRedirect("home");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+	
+//    public function cari(String condition, String params){
+//        if(!$this->_fields){
+//            $this->deskripsi();
+//        }
+//        
+//        $db = $this->getCheckedDBConnection();    
+//        $sql = "SELECT * FROM `{$this->_tableName}` WHERE {$condition} LIMIT 1";
+//        $stmt = $db->prepare($sql);
+//        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+//        $stmt->execute($params);
+//        $result = $stmt->fetch();
+//
+//        if ($result == null) return null;
+//        foreach($result as $field=>$value){
+//            //field must present
+//            assert(isset($this->_fields[$field]));
+//            $this->_fields[$field]['changed'] = false;
+//            $this->_fields[$field]['value'] = $value;
+//        }
+//
+//        $this->_key = $this->_fields['id']['value'];
+//        $this->_keyField = 'id';
+//
+//
+//        return $this;
+//    }
+
+}
