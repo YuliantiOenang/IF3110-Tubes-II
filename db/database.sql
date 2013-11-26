be2bd3f0859af2b8154ec5f1300f1c3ba0226127
willyfh <willyfitrahendria@gmail.com>
2013-11-26 14:27:35 +0700

    update database.sql

--- db/database.sql
+++ db/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Inang: 127.0.0.1
--- Waktu pembuatan: 21 Nov 2013 pada 13.29
+-- Waktu pembuatan: 26 Nov 2013 pada 08.22
 -- Versi Server: 5.5.27
 -- Versi PHP: 5.4.7
 
@@ -31,39 +31,40 @@ CREATE TABLE IF NOT EXISTS `barang` (
   `NamaBarang` varchar(20) DEFAULT NULL,
   `Harga` int(11) DEFAULT NULL,
   `Kategori` varchar(20) DEFAULT NULL,
-  `Jumlah` int(11) DEFAULT NULL
+  `Jumlah` int(11) DEFAULT NULL,
+  `Deskripsi` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`IdBarang`, `NamaBarang`, `Harga`, `Kategori`, `Jumlah`) VALUES
-(1, 'DagingSapi', 80000, 'Daging', 10),
-(2, 'IceCream', 10000, 'Snack', 14),
-(3, 'ChickenNugget', 30000, 'Frozen Food', 13),
-(4, 'BayamItali', 5000, 'Sayuran', 7),
-(5, 'BerasCarrefour', 12000, 'Beras', 10),
-(6, 'DagingYak', 85000, 'Daging', 11),
-(7, 'BerasTajMahal', 25000, 'Beras', 9),
-(8, 'BerasHCSR04', 22000, 'Beras', 7),
-(9, 'BerasRaskin', 20000, 'Beras', 12),
-(10, 'DagingAyam', 122000, 'Daging', 2),
-(11, 'DagingDomba', 92000, 'Daging', 8),
-(12, 'PetaiCHINA', 2500, 'Sayuran', 6),
-(13, 'SeledriHongkong', 4500, 'Sayuran', 12),
-(14, 'TimunAustralia', 14500, 'Sayuran', 5),
-(15, 'SOZZZZZZIS', 32000, 'Frozen Food', 25),
-(16, 'FIESTANIGGA', 22000, 'Frozen Food', 15),
-(17, 'BAKSOSOGOOD', 28000, 'Frozen Food', 42),
-(18, 'Beras A-KING', 200, 'Beras', 19),
-(19, 'DagingBebek', 15000, 'Daging', 10),
-(20, 'SiomayMantap', 20500, 'Frozen Food', 10),
-(21, 'Kangkung', 5300, 'Sayuran', 10),
-(22, 'ToKaeNoi', 5400, 'Snack', 10),
-(23, 'LeoKripikKentang', 3100, 'Snack', 10),
-(24, 'MiGemez', 1200, 'Snack', 24),
-(25, 'Bengbeng', 2050, 'Snack', 10);
+INSERT INTO `barang` (`IdBarang`, `NamaBarang`, `Harga`, `Kategori`, `Jumlah`, `Deskripsi`) VALUES
+(1, 'DagingSapi', 80000, 'Daging', 10, NULL),
+(2, 'IceCream', 10000, 'Snack', 14, NULL),
+(3, 'ChickenNugget', 30000, 'Frozen Food', 13, NULL),
+(4, 'BayamItali', 5000, 'Sayuran', 7, NULL),
+(5, 'BerasCarrefour', 11000, 'Beras', 10, NULL),
+(6, 'DagingYak', 85000, 'Daging', 11, NULL),
+(7, 'BerasTajMahal', 25000, 'Beras', 9, NULL),
+(8, 'BerasHCSR04', 22000, 'Beras', 7, NULL),
+(9, 'BerasRaskin', 20000, 'Beras', 12, NULL),
+(10, 'DagingAyam', 122000, 'Daging', 2, NULL),
+(11, 'DagingDomba', 92000, 'Daging', 8, NULL),
+(12, 'PetaiCHINA', 2500, 'Sayuran', 6, NULL),
+(13, 'SeledriHongkong', 4500, 'Sayuran', 12, NULL),
+(14, 'TimunAustralia', 14500, 'Sayuran', 5, NULL),
+(15, 'SOZZZZZZIS', 32000, 'Frozen Food', 25, NULL),
+(16, 'FIESTANIGGA', 22000, 'Frozen Food', 15, NULL),
+(17, 'BAKSOSOGOOD', 28000, 'Frozen Food', 42, NULL),
+(18, 'BerasA-KING', 200, 'Beras', 19, NULL),
+(19, 'DagingBebek', 15000, 'Daging', 10, NULL),
+(20, 'SiomayMantap', 20500, 'Frozen Food', 10, NULL),
+(21, 'Kangkung', 5300, 'Sayuran', 10, NULL),
+(22, 'ToKaeNoi', 5400, 'Snack', 10, NULL),
+(23, 'LeoKripikKentang', 3100, 'Snack', 10, NULL),
+(24, 'MiGemez', 1200, 'Snack', 24, NULL),
+(25, 'Bengbeng', 2050, 'Snack', 10, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,63 +92,63 @@ INSERT INTO `creditcard` (`number`, `name`, `expiredate`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `transactionlog` (
-  `IdBarang` int(11) DEFAULT NULL,
   `NamaBarang` varchar(20) DEFAULT NULL,
   `Harga` int(11) DEFAULT NULL,
   `Jumlah` int(11) DEFAULT NULL,
   `Username` varchar(20) DEFAULT NULL,
-  `Kategori` varchar(20) DEFAULT NULL
+  `Kategori` varchar(20) DEFAULT NULL,
+  `DetailPesanan` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `transactionlog`
 --
 
-INSERT INTO `transactionlog` (`IdBarang`, `NamaBarang`, `Harga`, `Jumlah`, `Username`, `Kategori`) VALUES
-(1, 'DagingSapi', 80000, 3, 'budie', 'Daging'),
-(1, 'DagingSapi', 80000, 2, 'budie', 'Daging'),
-(2, 'IceCream', 10000, 3, 'budie', 'Snack'),
-(2, 'IceCream', 10000, 4, 'budie', 'Snack'),
-(3, 'ChickenNugget', 30000, 1, 'budie', 'Frozen Food'),
-(3, 'ChickenNugget', 30000, 1, 'budie', 'Frozen Food'),
-(4, 'BayamItali', 5000, 7, 'budie', 'Sayuran'),
-(5, 'BerasCarrefour', 12000, 2, 'budie', 'Beras'),
-(6, 'DagingYak', 85000, 7, 'budie', 'Daging'),
-(6, 'DagingYak', 85000, 5, 'budie', 'Daging'),
-(6, 'DagingYak', 85000, 5, 'budie', 'Daging'),
-(5, 'BerasCarrefour', 12000, 3, 'budie', 'Beras'),
-(7, 'BerasTajMahal', 25000, 5, 'budie', 'Beras'),
-(1, 'DagingSapi', 80000, 2, 'budie', 'Daging'),
-(10, 'DagingAyam', 122000, 3, 'budie', 'Daging'),
-(4, 'BayamItali', 5000, 2, 'budie', 'Sayuran'),
-(12, 'PetaiCHINA', 2500, 3, 'budie', 'Sayuran'),
-(3, 'ChickenNugget', 30000, 2, 'budie', 'Frozen Food'),
-(5, 'BerasCarrefour', 12000, 1, 'Boci', 'Beras'),
-(2, 'IceCream', 10000, 2, 'budie', 'Snack'),
-(5, 'BerasCarrefour', 12000, 2, 'budie', 'Beras'),
-(3, 'ChickenNugget', 30000, 2, 'budie', 'Frozen Food'),
-(4, 'BayamItali', 5000, 2, 'yuhustore', 'Sayuran'),
-(1, 'DagingSapi', 80000, 1, 'budie', 'Daging'),
-(4, 'BayamItali', 5000, 1, 'budie', 'Sayuran'),
-(5, 'BerasCarrefour', 12000, 1, 'budie', 'Beras'),
-(18, 'Beras A-KING', 200, 3, 'budie', 'Beras'),
-(4, 'BayamItali', 5000, 4, 'budie', 'Sayuran'),
-(2, 'IceCream', 10000, 1, 'budie', 'Snack'),
-(1, 'DagingSapi', 80000, 1, 'budie', 'Daging'),
-(1, 'DagingSapi', 80000, 20, 'budie', 'Daging'),
-(6, 'DagingYak', 85000, 1, 'aditya2', 'Daging'),
-(1, 'DagingSapi', 80000, 1, 'aditya2', 'Daging'),
-(4, 'BayamItali', 5000, 1, 'budie', 'Sayuran'),
-(8, 'BerasHCSR04', 22000, 1, 'Budie', 'Beras'),
-(9, 'BerasRaskin', 20000, 1, 'Budie', 'Beras'),
-(21, 'Kangkung', 5300, 1, 'Budie', 'Sayuran'),
-(21, 'Kangkung', 5300, 1, 'Budie', 'Sayuran'),
-(14, 'TimunAustralia', 14500, 1, 'Budie', 'Sayuran'),
-(23, 'LeoKripikKentang', 3100, 1, 'Budie', 'Snack'),
-(2, 'IceCream', 10000, 1, 'Budie', 'Snack'),
-(25, 'Bengbeng', 2050, 2, 'Budie', 'Snack'),
-(20, 'SiomayMantap', 20500, 1, 'Budie', 'Frozen Food'),
-(17, 'BAKSOSOGOOD', 28000, 1, 'Budie', 'Frozen Food');
+INSERT INTO `transactionlog` (`NamaBarang`, `Harga`, `Jumlah`, `Username`, `Kategori`, `DetailPesanan`) VALUES
+('DagingSapi', 80000, 3, 'budie', 'Daging', NULL),
+('DagingSapi', 80000, 2, 'budie', 'Daging', NULL),
+('IceCream', 10000, 3, 'budie', 'Snack', NULL),
+('IceCream', 10000, 4, 'budie', 'Snack', NULL),
+('ChickenNugget', 30000, 1, 'budie', 'Frozen Food', NULL),
+('ChickenNugget', 30000, 1, 'budie', 'Frozen Food', NULL),
+('BayamItali', 5000, 7, 'budie', 'Sayuran', NULL),
+('BerasCarrefour', 12000, 2, 'budie', 'Beras', NULL),
+('DagingYak', 85000, 7, 'budie', 'Daging', NULL),
+('DagingYak', 85000, 5, 'budie', 'Daging', NULL),
+('DagingYak', 85000, 5, 'budie', 'Daging', NULL),
+('BerasCarrefour', 12000, 3, 'budie', 'Beras', NULL),
+('BerasTajMahal', 25000, 5, 'budie', 'Beras', NULL),
+('DagingSapi', 80000, 2, 'budie', 'Daging', NULL),
+('DagingAyam', 122000, 3, 'budie', 'Daging', NULL),
+('BayamItali', 5000, 2, 'budie', 'Sayuran', NULL),
+('PetaiCHINA', 2500, 3, 'budie', 'Sayuran', NULL),
+('ChickenNugget', 30000, 2, 'budie', 'Frozen Food', NULL),
+('BerasCarrefour', 12000, 1, 'Boci', 'Beras', NULL),
+('IceCream', 10000, 2, 'budie', 'Snack', NULL),
+('BerasCarrefour', 12000, 2, 'budie', 'Beras', NULL),
+('ChickenNugget', 30000, 2, 'budie', 'Frozen Food', NULL),
+('BayamItali', 5000, 2, 'yuhustore', 'Sayuran', NULL),
+('DagingSapi', 80000, 1, 'budie', 'Daging', NULL),
+('BayamItali', 5000, 1, 'budie', 'Sayuran', NULL),
+('BerasCarrefour', 12000, 1, 'budie', 'Beras', NULL),
+('BerasA-KING', 200, 3, 'budie', 'Beras', NULL),
+('BayamItali', 5000, 4, 'budie', 'Sayuran', NULL),
+('IceCream', 10000, 1, 'budie', 'Snack', NULL),
+('DagingSapi', 80000, 1, 'budie', 'Daging', NULL),
+('DagingSapi', 80000, 20, 'budie', 'Daging', NULL),
+('DagingYak', 85000, 1, 'aditya2', 'Daging', NULL),
+('DagingSapi', 80000, 1, 'aditya2', 'Daging', NULL),
+('BayamItali', 5000, 1, 'budie', 'Sayuran', NULL),
+('BerasHCSR04', 22000, 1, 'Budie', 'Beras', NULL),
+('BerasRaskin', 20000, 1, 'Budie', 'Beras', NULL),
+('Kangkung', 5300, 1, 'Budie', 'Sayuran', NULL),
+('Kangkung', 5300, 1, 'Budie', 'Sayuran', NULL),
+('TimunAustralia', 14500, 1, 'Budie', 'Sayuran', NULL),
+('LeoKripikKentang', 3100, 1, 'Budie', 'Snack', NULL),
+('IceCream', 10000, 1, 'Budie', 'Snack', NULL),
+('Bengbeng', 2050, 2, 'Budie', 'Snack', NULL),
+('SiomayMantap', 20500, 1, 'Budie', 'Frozen Food', NULL),
+('BAKSOSOGOOD', 28000, 1, 'Budie', 'Frozen Food', NULL);
 
 -- --------------------------------------------------------
 
@@ -177,9 +178,11 @@ INSERT INTO `user` (`username`, `password`, `email`, `namalengkap`, `nohp`, `pro
 ('aditya', '12345678', 'adt@adit.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', '12341234'),
 ('aditya2', '12345678', 'aditya@yahoo.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', '12341234'),
 ('aditya3', '12345678', 'aditya3@yahoo.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', '12341234'),
+('admin', 'admin123', 'admin@admin.com', 'admin admin', '0', '-', '-', '-', '0', NULL),
 ('bebek', '12345678', 'bebek@bebek.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', NULL),
-('budie', '12341234', 'budie@gmail.com', 'budi ebudi', '087899', 'Jawa Timur', 'Surabaya', 'Surabaya', '5000', '12341234'),
+('budie', '12341234', 'budie@gmail.com', 'ebudi ebudi', '087899', 'Jawa Timur', 'Surabaya', 'Surabaya', '5000', '12341234'),
 ('budih', '12345678', 'budie@budi.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', '12341234'),
+('cobabaru', '123123123', 'coba@baru.com', 'coba baru', '0', '-', '-', '-', '0', '12341234'),
 ('cobabro', '12345678', 'coba@bro.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', NULL),
 ('cobadulu', '12345678', 'coba@dulu.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', '12341234'),
 ('cobalagi', '12345678', 'cobalagi@lagi.com', 'budi doang', '2134', 'Jawa Barat', 'Bandung', 'Bandung', '4001', NULL),
