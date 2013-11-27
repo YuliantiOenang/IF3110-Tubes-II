48e8d6f399b470b8e34644c5fc98c87be9cb24ef
harits <adilelfahmi@gmail.com>
2013-11-27 13:56:58 +0700

    view cart done

--- WebContent/viewCart.jsp
+++ WebContent/viewCart.jsp
@@ -5,16 +5,61 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>View Cart</title>
 </head>
 <body>
+	<div class="page_container">
+	<%@ include file="template/template.jsp" %> 
 	<%
-		ArrayList<Barang> barangs = (ArrayList<Barang>) request.getAttribute("cart");
-		for(Barang b: barangs){
+		int totalHarga = (Integer) request.getAttribute("total");
 		%>
-			<div>Nama: <%= b.getNama() %></div>
+			<h1>View Cart</h1>
+			<h1>Total Harga: <%= totalHarga %></h1>
+		<%
+		ArrayList<Barang> barangs = (ArrayList<Barang>) request.getAttribute("barangs");
+		int awal = (request.getParameter("start") == null ? 0 : Integer.parseInt(request.getParameter("start")));
+		int no = 0;
+		if(barangs != null){
+			for(Barang b: barangs){
+				if(no < awal){
+					no++;
+					continue;
+				}			
+			%>
+				<div class="list_barang">
+					<div class="gambar">
+						<img src="res/<%= b.getGambar() %>" width="150px" height="150px">
+					</div>
+					<div class="list_desc">
+						<strong>Nama:</strong> <a href="detail?gid=<%= b.getId_inven() %>"><%= b.getNama() %></a><br/>
+						<strong>Harga:</strong> <%= b.getHarga() %><br/><br/>
+					</div>
+				</div>
+			<% 
+				if(no - awal == 9){
+					break;
+				}
+				no++;
+			}
+			%>
+			<div class="pagination"> Halaman: 
+				<%
+				for(int i=0;i<Math.ceil(barangs.size()/10f);i++){
+					if(i * 10 == awal){
+						out.println("[" + (i+1) + "]");
+					} else {
+						%>	
+						<a href="viewCart?start=<%= i*10 %>"><%= (i+1) %></a>
+						<%
+					}
+				}
+				%>
+				</div>
 		<% 
+		} else {
+			
 		}
-	%>
+		%>
+	</div>
 </body>
 </html>
--- src/ViewCart.java
+++ src/ViewCart.java
@@ -1,6 +1,10 @@
 
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -8,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import kelas.Barang;
+import kelas.Database;
 
 /**
  * Servlet implementation class ViewCart
@@ -27,32 +34,51 @@ public class ViewCart extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(true);
+		String db = "toko_imba";
+		java.sql.Connection con = null;
+		ArrayList<Integer> cart = null;
+		int total = 0;
+		ArrayList<Barang> barangs = new ArrayList<Barang>();
+		if(session.getAttribute("cart") != null){
+			try {
+				Class.forName("org.gjt.mm.mysql.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost/"+db, Database.getUser(), Database.getPass());
+				
+				cart = (ArrayList<Integer>) session.getAttribute("cart");
+				
+				for(Integer barangIndex: cart){
+					Statement state = con.createStatement();
+					ResultSet rs = state.executeQuery("SELECT * FROM inventori JOIN kategori ON inventori.id_kategori = kategori.id_kategori AND inventori.id_inventori = \"" + barangIndex + "\"");
+					
+					while(rs.next()){
+						String name = rs.getString("nama_inventori");
+						Barang brg = new Barang(name);
+						brg.setId_cat(rs.getInt("id_kategori"));
+						brg.setId_inven(rs.getInt("id_inventori"));
+						brg.setDesc(rs.getString("description"));
+						brg.setHarga(rs.getInt("harga"));
+						total += brg.getHarga();
+						brg.setGambar(rs.getString("gambar"));
+						brg.setJumlah(rs.getInt("jumlah"));
+						barangs.add(brg);
+					}
+				}
+			}
+			catch(SQLException | ClassNotFoundException e) {
+				System.out.println("SQLException caught: " +e.getMessage());
+			}
+		}
+		request.setAttribute("total", total);
+		request.setAttribute("barangs", barangs);
+		request.getRequestDispatcher("viewCart.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String db = "toko_imba";
-		java.sql.Connection con = null;
-		int id = Integer.parseInt(request.getParameter("id_barang"));
 		
-		ArrayList<Integer> cart;
-		
-		HttpSession session = request.getSession(true);
-		if(session.getAttribute("cart") == null){
-			cart = new ArrayList<Integer>();
-			cart.add(id);
-			session.setAttribute("cart", cart);
-		} else {
-			cart = (ArrayList<Integer>) session.getAttribute("cart");
-			cart.add(id);
-			session.setAttribute("cart", cart);
-		}
-		
-		request.setAttribute("cart", cart);
-		request.getRequestDispatcher("viewCart.jsp").forward(request, response);
 	}
 
 }
