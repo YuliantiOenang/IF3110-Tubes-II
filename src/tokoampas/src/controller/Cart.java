608a55fefd75bc1a62eb45df6a9dbdc678ef935e
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-11-27 16:18:35 +0700

    Add cart

--- src/tokoampas/src/controller/Cart.java
+++ src/tokoampas/src/controller/Cart.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +29,18 @@ public class Cart extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String forward="";
+
+	    Map parameters = request.getParameterMap();
+	    if (parameters.containsKey("add")){
+	      forward = "/view/Cart.jsp";
+	    } else if (parameters.containsKey("checkout")){
+	      forward = "/view/Cart.jsp";
+	    } else {
+	      forward = "/view/Cart.jsp";
+	    }
+	    RequestDispatcher view = request.getRequestDispatcher(forward);
+	    view.forward(request, response);
 	}
 
 	/**
