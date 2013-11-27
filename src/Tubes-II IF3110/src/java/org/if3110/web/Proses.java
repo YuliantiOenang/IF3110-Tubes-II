ae122031ef0a62ec9fab07e5205623b26dbb2f86
Setyo Legowo <gw.tio145@gmail.com>
2013-11-27 16:09:12 +0700

    100% jsonresponder. Not tested.

--- src/Tubes-II IF3110/src/java/org/if3110/web/Proses.java
+++ src/Tubes-II IF3110/src/java/org/if3110/web/Proses.java
@@ -10,11 +10,19 @@ package org.if3110.web;
  */
 public class Proses {
     private int __id_num;
-    public void pendaftaranNew(String nama_pengguna, String kata_sandi, String nama_lengkap, String email)
+    public void pendaftaranNew(String nama_pengguna, String kata_sandi,
+        String nama_lengkap, String email)
     {
         
     }
     public int getUserID() {
         return __id_num;
+    }
+    
+    public void daftarKartuKredit(String user_id, 
+        String nomor_kartu, String pemilik, String bulan_exp,
+        String tahun_exp)
+    {
+        
     }
 }
