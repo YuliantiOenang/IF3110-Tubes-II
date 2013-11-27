4646c54052a0bdf0a1bed300511b25b19d5bd577
unknown <fathan.pranaya@gmail.com>
2013-11-27 18:26:48 +0700

    bisa nambah barang(fix)

---
+++ src/servlets/barang.java
@@ -0,0 +1,98 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Barang;
+
+@WebServlet("/barang")
+public class barang extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String id_barang;
+	private String nama_barang;
+	private String gambar_barang;
+	private String harga_barang;
+	private String kategori_barang;
+	private String n_beli;
+	private String keterangan;
+	private String stok;
+
+    public barang() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		id_barang = session.getAttribute("n_barang").toString();
+		nama_barang = request.getParameter("nama_barang");
+		harga_barang = request.getParameter("harga_barang");
+		kategori_barang = request.getParameter("kategori_barang");
+		n_beli = request.getParameter("n_beli");
+		keterangan = request.getParameter("keterangan");
+		stok = request.getParameter("stok");
+		gambar_barang = request.getParameter("gambar_barang");
+		
+		Connection conn = null;
+		Statement stmt = null;
+		/*String query = "INSERT INTO `progin_13511021`.`barang` (`id_barang`, `nama_barang`, `gambar_barang`, `harga_barang`, `kategori_barang`, `n_beli`, `keterangan`, `stok`)"+ 
+				       "VALUES ('"+id_barang+"', '"+nama_barang+"', '', '"+harga_barang+"', '"+kategori_barang+"', '"+n_beli+"', '"+keterangan+"', '"+stok+"')";
+		*/
+		String query = "INSERT INTO barang (id_barang, nama_barang, gambar_barang, harga_barang, kategori_barang, n_beli, keterangan, stok) VALUES ('"
+				+ id_barang
+				+ "','"
+				+ nama_barang
+				+ "','"
+				+ "./images/Tahu.png"
+				+ "','"
+				+ harga_barang
+				+ "','"
+				+ kategori_barang
+				+ "','"
+				+ n_beli
+				+ "','"
+				+ keterangan
+				+ "','"
+				+ stok + "')";
+		System.out.print(query);
+		
+		try {
+			String userName = "root";
+			String password = "";
+			String url = "jdbc:mysql://localhost/progin_13511021";
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(url, userName, password);
+		} catch (Exception e) {
+			System.out.println("Cannot connect to database server");
+		}
+		
+		try {
+			stmt = conn.createStatement();
+			stmt.executeUpdate(query);
+			stmt.close();
+			conn.close();
+			System.out.println("SUkses");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher view = request.getRequestDispatcher("/sukses.jsp");
+		view.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
--- src/servlets/index.java
+++ src/servlets/index.java
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bean.Barang;
 
@@ -26,15 +27,15 @@ public class index extends HttpServlet {
 	private ArrayList<Barang> elektronik = new ArrayList<Barang>();
 	private ArrayList<Barang> rumahtangga = new ArrayList<Barang>();
 	private ArrayList<Barang> olahraga = new ArrayList<Barang>();
-	private ArrayList<String> kategori_url = new ArrayList<String>();
+	private int n_barang;
 
 	public index() {
 	}
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		PrintWriter out = response.getWriter();
 		Connection conn = null;
-		kategori_url.clear();
 		Statement stmt[] = new Statement[6];
 		for (int i = 0; i < 6; i++)
 			stmt[i] = null;
@@ -52,14 +53,14 @@ public class index extends HttpServlet {
 		ResultSet rs_elektronik = null;
 		ResultSet rs_rumahtangga = null;
 		ResultSet rs_olahraga = null;
-		ResultSet rs_kategori = null;
+		ResultSet rs = null;
 
 		String q_pangan = "SELECT * FROM `progin_13511021`.barang WHERE kategori_barang = 1 ORDER BY n_beli DESC LIMIT 0,3";
 		String q_pakaian = "SELECT * FROM `progin_13511021`.barang WHERE kategori_barang = 2 ORDER BY n_beli DESC LIMIT 0,3";
 		String q_elektronik = "SELECT * FROM `progin_13511021`.barang WHERE kategori_barang = 3 ORDER BY n_beli DESC LIMIT 0,3";
 		String q_rumahtangga = "SELECT * FROM `progin_13511021`.barang WHERE kategori_barang = 4 ORDER BY n_beli DESC LIMIT 0,3";
 		String q_olahraga = "SELECT * FROM `progin_13511021`.barang WHERE kategori_barang = 5 ORDER BY n_beli DESC LIMIT 0,3";
-		String q_kategori = "SELECT DISTINCT kategori_barang FROM `progin_13511021`.barang";
+		String q = "SELECT COUNT(nama_barang) from `barang`";
 		
 		try {
 			for (int i = 0; i < 6; i++)
@@ -69,7 +70,7 @@ public class index extends HttpServlet {
 			rs_elektronik = stmt[2].executeQuery(q_elektronik);
 			rs_rumahtangga = stmt[3].executeQuery(q_rumahtangga);
 			rs_olahraga = stmt[4].executeQuery(q_olahraga);
-			rs_kategori = stmt[5].executeQuery(q_kategori);
+			rs = stmt[5].executeQuery(q);
 
 			while (rs_pangan.next()) {
 				Barang temp = new Barang(rs_pangan);
@@ -96,18 +97,8 @@ public class index extends HttpServlet {
 				olahraga.add(temp);
 			}
 			
-			while (rs_kategori.next()) {
-				int idx = Integer.parseInt(rs_kategori.getObject(1).toString());
-				if (idx == 1) 
-					kategori_url.add("<li> <a href='kategori?id="+idx+"&page="+1+"&order=nama_barang'>"+"Pangan</a></li>");
-				else if (idx == 2) 
-					kategori_url.add("<li> <a href='kategori?id="+idx+"&page="+1+"&order=nama_barang'>"+"Pakaian</a></li>");
-				else if (idx == 3) 
-					kategori_url.add("<li> <a href='kategori?id="+idx+"&page="+1+"&order=nama_barang'>"+"Elektronik</a></li>");
-				else if (idx == 4) 
-					kategori_url.add("<li> <a href='kategori?id="+idx+"&page="+1+"&order=nama_barang'>"+"Rumah Tangga</a></li>");
-				else if (idx == 5) 
-					kategori_url.add("<li> <a href='kategori?id="+idx+"&page="+1+"&order=nama_barang'>"+"Olah Raga</a></li>");				
+			while (rs.next()) {
+				n_barang = Integer.parseInt(rs.getObject(1).toString());	
 			}
 			
 			rs_pangan.close();
@@ -115,7 +106,7 @@ public class index extends HttpServlet {
 			rs_elektronik.close();
 			rs_rumahtangga.close();
 			rs_olahraga.close();
-			rs_kategori.close();
+			rs.close();
 			
 			for (int i = 0; i < 6; i++)
 				stmt[i].close();
@@ -129,7 +120,8 @@ public class index extends HttpServlet {
 		request.setAttribute("elektronik", elektronik);
 		request.setAttribute("rumahtangga", rumahtangga);
 		request.setAttribute("olahraga", olahraga);
-		request.setAttribute("kategori", kategori_url);
+		session.setAttribute("n_barang", n_barang+1000);
+		System.out.println("n_barang : "+n_barang);
 		RequestDispatcher view = request.getRequestDispatcher("/index.jsp");
 		view.forward(request, response);
 	}
