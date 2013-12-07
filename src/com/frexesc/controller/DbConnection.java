3e9683bcd7961214463c5343dbc2f6403b7e1712
reximir <davidlibraboy@gmail.com>
2013-12-07 14:28:31 +0700

    revert back

--- src/com/frexesc/controller/DbConnection.java
+++ src/com/frexesc/controller/DbConnection.java
@@ -2,15 +2,14 @@ package com.frexesc.controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
 public class DbConnection {
 	Connection connection = null;
 
 	public Connection mySqlConnection() {
-		String url = "jdbc:mysql://localhost:3306/frexesc";
+		String url = "jdbc:mysql://10.0.28.130:3306/d49533220d59549ea8d59a42dd0c22251";
 		String driver = "com.mysql.jdbc.Driver";
-		String username = "root";
-		String password = "";
+		String username = "uq4KXgZdPVKL4";
+		String password = "pUxhgVj9EKMdX";
 
 		try {
 			Class.forName(driver).newInstance();
